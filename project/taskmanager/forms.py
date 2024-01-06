f5b65ee2bfdac00ef221a5c2293479ff47bdcc04
hieuhn0301 <hieuhn0301@gmail.com>
2024-01-06 23:50:42 +0700

    fix

--- project/taskmanager/forms.py
+++ project/taskmanager/forms.py
@@ -8,22 +8,6 @@ from .models import Task
 from .widgets import InputCustom, TextareaCustom
 
 
-# class TaskForm(forms.ModelForm):
-#     class Meta:
-#         model = Task
-#         exclude = ['__all__']
-#         widgets = {
-#             'title': InputCustom(attrs={'placeholder': 'Task', 'title': 'Task'}),
-#             'description': TextareaCustom(attrs={'placeholder': 'Description', 'title': 'Description'})
-#         }
-#
-#     def save(self, commit=True, *args, **kwargs):
-#         m = super().save(commit=False)
-#         m.modified_at = timezone.now()
-#         if commit:
-#             m.save()
-#         return m
-
 class TaskForm(forms.Form):
     # user = forms.ForeignKey()
     new_task_id = forms.IntegerField(required=False)
